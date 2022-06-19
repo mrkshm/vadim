831016426b4981555d0b9e5cd0c799865dba45a6
mrks <hey@mrks.me>
2022-06-19 18:40:32 +0200

    fixing css error

--- src/routes/mentionslegales.svelte
+++ src/routes/mentionslegales.svelte
@@ -35,7 +35,7 @@
 
 	<div class="mentions">
 		<p>Le site vadimsher.com est publié par Vadim Sher.</p>
-		<p>Il a été conçu par <a href="mrks.me">mrks.me.</a></p>
+		<p>Il a été conçu par <a href="https://mrks.me">mrks.me.</a></p>
 		<p class="hebergement">Hébergement : <br />mrks.me<br />59, de l'Aqueduc<br />75010 Paris</p>
 	</div>
 </div>
